f342190e9a157d82524b91fa412aa15c2209f589
CReshCreates <reshrajchaudhary009@gmail.com>
2026-03-26 23:03:42 +0545

    cashier frontend ra backend connect vayo

---
+++ javascript/cashier.js
@@ -0,0 +1,226 @@
+const token = localStorage.getItem("token");
+if(!token){
+    window.location.replace("login.html");
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    loadCashiers();
+    loadStats();
+})
+
+window.addCashier = function() {
+    document.getElementById("popup").style.display = "block";
+    document.getElementById("form-title").innerText = "Add Cashier";
+    document.querySelector('#username-field').style.display = "block";
+    document.querySelector('#password-field').style.display = "block";
+    clearForm();
+    editId = null;
+}
+
+window.closePopup = function(){
+    document.getElementById("popup").style.display = "none";
+}
+
+window.saveCashier = async function (){
+    let name = document.getElementById("name").value;
+    let address = document.getElementById("address").value;
+    let phone = document.getElementById("phone").value;
+    let shift = document.getElementById("shift").value;
+    let status = document.getElementById("status").value;
+    let username = document.getElementById("username").value;
+    let password = document.getElementById('password').value;
+
+    if(!name || !phone || !address || !shift || !status){
+        alert("Please fill all the details!!!");
+        return;
+    }
+
+    const data = {name, address, phone, shift, status};
+
+    let response;
+
+    try{
+        if(editId) {
+            response = await fetch(`${BASE_URL}/admin/update/${editId}`,{
+                method: "PUT",
+                headers: getHeaders(),
+                body: JSON.stringify(data)
+            });
+        }else{
+            if(!username){
+                alert("Username is required!");
+                return;
+            }
+            response = await fetch(`${BASE_URL}/admin/registerUser`,{
+                method: "POST",
+                headers: getHeaders(),
+                body : JSON.stringify({
+                user: {
+                    username: username,
+                    role: "CASHIER",
+                    password: password
+                },
+                cashier: {
+                    name, address, phone, shift, status
+                }})
+            });
+
+            if (!response.ok) {
+                const msg = await response.text();
+                alert("Error: " + msg);
+                return;
+            }
+        }
+        closePopup();
+        clearForm();
+
+        await loadCashiers();
+        await loadStats();
+    }catch(err){
+        console.log(err);
+    }
+};
+
+window.handleDropdown = function (select) {
+    if (select.value === "logout") {
+        localStorage.removeItem("token");
+        window.location.href = "login.html";
+    }
+};
+
+const BASE_URL = "http://localhost:8080";
+
+let cashiers = [];
+let editId = null;
+
+function getHeaders(){
+    const token = localStorage.getItem("token");
+
+    return {
+        "Content-Type" : "application/json",
+        "Authorization" : `Bearer ${token}`
+    };
+}
+
+
+
+async function loadCashiers(){
+    try{
+        const response = await fetch(`${BASE_URL}/admin/getAllCashiers`,{
+            headers: getHeaders()
+        });
+
+        if(response.status == 204){
+            cashiers = [];
+        }else{
+            cashiers = await response.json();
+        }
+
+        renderTable();
+    }catch(err){
+        console.assert(err);
+    }
+}
+
+async function loadStats(){
+    try{
+        const total = await fetch(`${BASE_URL}/admin/getAllSize`, {headers: getHeaders()}).then(r => r.json());
+        const active = await fetch(`${BASE_URL}/admin/getActiveSize`, {headers: getHeaders()}).then(r => r.json());
+        const inactive = await fetch(`${BASE_URL}/admin/getInactiveSize`, {headers: getHeaders()}).then(r => r.json());
+        const onShift = await fetch(`${BASE_URL}/admin/getOnShiftCashiers`, {headers: getHeaders()}).then(r => r.json());
+
+
+        document.querySelector('.total').innerText = total;
+        document.querySelector('.actives').innerText = active;
+        document.querySelector('.inactive').innerText = inactive;
+        document.querySelector('.onshift').innerText = onShift;
+
+    }catch(err){
+        console.log(err);
+    }
+}
+
+function renderTable() {
+    let tbody = document.querySelector("#table tbody");
+    tbody.innerHTML = "";
+
+    
+
+    cashiers.forEach(cashier => {
+        let statusClass = cashier.status.toLowerCase() === "active" ? "active-status" : "inactive-status";
+
+
+        let row = `
+            <tr>
+                <td>${cashier.cashierId}</td>
+                <td>${cashier.name}</td>
+                <td>${cashier.address}</td>
+                <td>${cashier.phone}</td>
+                <td>${cashier.shift}</td>
+                <td><span class="${statusClass}">${cashier.status}</span></td>
+                <td>
+                    <button class = "edit-btn" data-id="${cashier.cashierId}">Edit</button>
+                    <button class = "delete-btn" data-id="${cashier.cashierId}">Delete</button>
+                </td>
+            </tr>
+        `;
+
+        tbody.innerHTML +=row;
+    })
+
+    attachEvents();
+}
+
+function attachEvents(){
+    document.querySelectorAll('.edit-btn').forEach(btn =>{
+        btn.addEventListener("click", () => editCashier(btn.dataset.id));
+    });
+
+    
+    document.querySelectorAll('.delete-btn').forEach(btn =>{
+        btn.addEventListener("click", () => deleteCashier(btn.dataset.id));
+    });
+}
+
+
+
+function clearForm(){
+    document.getElementById("name").value = "";
+    document.getElementById("address").value = "";
+    document.getElementById("phone").value = "";
+    document.getElementById("shift").value = "";
+    document.getElementById("status").value = "";
+}
+
+
+
+async function deleteCashier(id){
+    if(!confirm("Are you sure?")) return;
+
+    await fetch(`${BASE_URL}/admin/delete/${id}`,{
+        method: "DELETE",
+        headers: getHeaders()
+    });
+
+    loadCashiers();
+    loadStats();
+}
+
+function editCashier(id) {
+    const c = cashiers.find(x => x.cashierId == id);
+    document.getElementById("name").value = c.name;
+    document.getElementById("address").value = c.address;
+    document.getElementById("phone").value = c.phone;
+    document.getElementById("shift").value = c.shift;
+    document.getElementById("status").value = c.status;
+
+    document.querySelector('#username-field').style.display = "none";
+    document.querySelector('#password-field').style.display = "none";
+
+    editId = id;
+
+    document.getElementById("form-title").innerText = "Update Cashier";
+    document.getElementById("popup").style.display = "block";
+
+}
+
---
+++ javascript/login.js
@@ -0,0 +1,28 @@
+const BASE_URL = "http://192.168.1.75:8080";
+
+async function login() {
+  const username = document.querySelector('#username').value;
+  const password = document.querySelector('#pass').value;
+
+  const response = await fetch(`${BASE_URL}/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ username, password })
+  });
+
+  const data = await response.json();
+  localStorage.setItem("token", data.token);
+  localStorage.setItem("role", data.role);
+
+  if(data.role === "ROLE_ADMIN"){
+    window.location.href = "/pages/sales.html";
+  }else if(data.role === "ROLE_CASHIER"){
+    window.location.href = ("/pages/cash.html")
+  }else{
+    window.alert("Unknown Role");
+  }
+
+  alert("Login success");
+}
